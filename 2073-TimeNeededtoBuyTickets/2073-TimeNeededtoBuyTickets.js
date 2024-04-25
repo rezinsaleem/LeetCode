3da771486d7afd63004743f22bebb2b2df50c03f
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-04-25 09:52:38 +0530

    Create file

---
+++ 2073-TimeNeededtoBuyTickets/2073-TimeNeededtoBuyTickets.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} tickets
+ * @param {number} k
+ * @return {number}
+ */
+var timeRequiredToBuy = function (tickets, k) {
+    let count = 0;
+    while (tickets[k] !== 0) {
+[2,3,2]
