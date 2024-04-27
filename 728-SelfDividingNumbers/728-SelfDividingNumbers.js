1d4bc4564d069988086bfdc624f9867d354f50f2
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-04-27 19:33:14 +0530

    Create file

---
+++ 728-SelfDividingNumbers/728-SelfDividingNumbers.js
@@ -0,0 +1,17 @@
+                val = false
+                break;
+            } else {
+                val = true;
+            }
+        }
+        if (val == true)
+            arr.push(i)
+    }
+            if (num[j] == 0 || i % num[j] != 0) {
+    arr = [];
+    for (i = left; i <= right; i++) {
+        num = i.toString().split('').map(Number)
+        for (j = 0; j < num.length; j++) {
+var selfDividingNumbers = function (left, right) {
+ */
+1
