7e47df44ecb4db097d1d60dc414f57b53df46711
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-01 17:22:36 +0530

    Create file

---
+++ 206-ReverseLinkedList/206-ReverseLinkedList.js
@@ -0,0 +1,13 @@
+    let prev = null;
+    let curr = head;
+    while (curr) {
+        let next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    }
+    head = prev
+    return head
+};
+var reverseList = function (head) {
+[
