8e89ca61dfffbda30dee674ca99fe8e15e4e07c5
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-02 19:29:57 +0530

    Create file

---
+++ 2441-LargestPositiveIntegerThatExistsWithItsNegative/2441-LargestPositiveIntegerThatExistsWithItsNegative.js
@@ -0,0 +1,17 @@
+                 flag=1;
+                 break
+                }
+         }
+     }
+     if(flag==1){
+         return num;
+     }else{
+                 num=nums[i]
+         for(j=0;j<nums.length;j++){
+             if(nums[i]*-1==nums[nums.length-j-1]){
+     for(i=0;i<nums.length;i++){
+    let flag=0;
+    let num
+    nums=nums.sort(function(a,b){return a-b})
+var findMaxK = function(nums) {
+[-1,2,-3,3]
