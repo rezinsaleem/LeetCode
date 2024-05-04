29069b73b1ccad198c668d55f952dd4125e7ee75
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-04 14:45:36 +0530

    Create file

---
+++ 75-SortColors/75-SortColors.js
@@ -0,0 +1,17 @@
+  do {
+      }
+    }    
+    swapped=false;
+    for(i=0;i<arr.length-1;i++){
+      if(arr[i]>arr[i+1]){
+        swapped=true;
+        temp = arr[i];
+        arr[i+1]=temp;
+        arr[i]=arr[i+1];
+  } while (swapped);
+  return arr;
+    let swapped;
+var sortColors = function(arr) {
+ */
+ * @return {void} Do not return anything, modify nums in-place instead.
+[
