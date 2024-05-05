4aaa4131f6be5892c1ba9b0ae3bb30ea2c1a2835
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-05 19:48:36 +0530

    Create file

---
+++ 237-DeleteNodeinaLinkedList/237-DeleteNodeinaLinkedList.js
@@ -0,0 +1,16 @@
+  node.val = node.next.val;
+var deleteNode = function(node) {
+ */
+ * @return {void} Do not return anything, modify node in-place instead.
+ * @param {ListNode} node
+/**
+ */
+ * }
+ *     this.next = null;
+ * function ListNode(val) {
+ *     this.val = val;
+/**
+ * Definition for singly-linked list.
+  node.next = node.next.next;
+};
+[
