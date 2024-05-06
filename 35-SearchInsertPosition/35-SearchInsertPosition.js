0807c028af748a172a6c217c53aa809da45f504e
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-06 12:22:48 +0530

    Create file

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.js
@@ -0,0 +1,17 @@
+        if (nums[mid] == target) {
+            return mid
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
+    let left = 0;
+        }
+        if (target > nums[mid]) {
+            left = mid + 1
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+    let right = nums.length - 1;
+        } else {
+[
