fe251a54b27eae082c75ccdef90b6aad90ef4a7e
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-07 18:26:46 +0530

    Create file

---
+++ 155-MinStack/155-MinStack.js
@@ -0,0 +1,14 @@
+    this.stack2.pop()
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+    return this.stack1[this.stack1.length - 1]
+    this.stack1.pop()
+MinStack.prototype.pop = function () {
+ */
+};
+
+[
