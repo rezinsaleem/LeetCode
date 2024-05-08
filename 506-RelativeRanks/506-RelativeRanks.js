dec2121b4ea1453f6024ed2f34a2dda4eac35d6c
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-08 10:20:57 +0530

    Create file

---
+++ 506-RelativeRanks/506-RelativeRanks.js
@@ -0,0 +1,14 @@
+        } else if (score[i] == arr[2]) {
+            answer.push("Bronze Medal")
+            answer.push("Gold Medal")
+        } else if (score[i] == arr[1]) {
+            answer.push("Silver Medal")
+        if (score[i] == arr[0]) {
+    for (i = 0; i < score.length; i++) {
+    let answer = []
+    arr = arr.sort((a, b) => b - a)
+    let arr = score.slice(0, score.length)
+var findRelativeRanks = function (score) {
+ */
+ * @return {string[]}
+[
