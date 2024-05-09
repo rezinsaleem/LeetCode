66b1c7a5a32da03ae275f2d20fa33a24a68542ca
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-09 10:37:00 +0530

    Create file

---
+++ 3075-MaximizeHappinessofSelectedChildren/3075-MaximizeHappinessofSelectedChildren.js
@@ -0,0 +1,11 @@
+        if (happiness[j] - j > 0) {
+    for (let j = 0; j < k; j++) {
+    happiness = happiness.sort((a, b) => b - a)
+    }
+    let sum = 0
+var maximumHappinessSum = function (happiness, k) {
+            sum += happiness[j] - j;
+        }
+    return sum
+};
+[
