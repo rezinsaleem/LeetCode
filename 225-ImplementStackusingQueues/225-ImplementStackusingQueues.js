bdb738760ccb9081a48b8d7cd7ee2f090e27b52a
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-10 00:22:41 +0530

    Create file

---
+++ 225-ImplementStackusingQueues/225-ImplementStackusingQueues.js
@@ -0,0 +1,14 @@
+    while (this.q1.length > 0) {
+    this.q2.push(x);
+MyStack.prototype.push = function (x) {
+ */
+ * @return {void}
+ * @param {number} x
+/** 
+
+};
+    this.q2 = []
+    this.q1 = []
+var MyStack = function () {
+        this.q2.push(this.q1.shift())
+[
