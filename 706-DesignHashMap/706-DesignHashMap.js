03a08bb44b4571a2211baa632385422efe070085
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-10 19:05:40 +0530

    Create file

---
+++ 706-DesignHashMap/706-DesignHashMap.js
@@ -0,0 +1,11 @@
+    delete this.hashMap[key];
+};
+
+/** 
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
+[
