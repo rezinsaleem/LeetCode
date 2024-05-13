9144ddf0d2ea405f62ca86cc7e6f24b2bfd72456
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-13 11:15:44 +0530

    Create file

---
+++ 15-3Sum/15-3Sum.js
@@ -0,0 +1,13 @@
+            } else if (nums[i] + nums[j] + nums[k] > 0) {
+                k--
+        let k = nums.length - 1
+        while (j < k) {
+            if (nums[i] + nums[j] + nums[k] < 0) {
+                j++
+        let j = i + 1;
+    for (let i = 0; i < nums.length - 2; i++) {
+    let obj = {}
+var threeSum = function (nums) {
+    nums = nums.sort((a, b) => a - b)
+ */
+[
