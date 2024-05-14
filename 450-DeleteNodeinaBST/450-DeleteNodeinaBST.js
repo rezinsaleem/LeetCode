246a2e063d95a6454fafa9ca5664e7d88175b61f
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-14 09:24:17 +0530

    Create file

---
+++ 450-DeleteNodeinaBST/450-DeleteNodeinaBST.js
@@ -0,0 +1,13 @@
+    if (!root) return null;
+var deleteNode = function (root, key) {
+}
+    }
+    } else {
+        return min(root.left)
+        return root.val
+    if (!root.left) {
+var min = function (root) {
+ */
+ * @return {TreeNode}
+ * @param {number} key
+[
