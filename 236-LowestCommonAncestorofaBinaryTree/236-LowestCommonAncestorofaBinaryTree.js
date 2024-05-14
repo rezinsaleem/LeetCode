abed63686ed482e42eece2d32d4a9656e0ade723
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-14 16:01:52 +0530

    Create file

---
+++ 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.js
@@ -0,0 +1,14 @@
+ */
+ * @return {TreeNode}
+ * @param {TreeNode} q
+ * @param {TreeNode} p
+ * @param {TreeNode} root
+/**
+ */
+ * }
+ *     this.left = this.right = null;
+ *     this.val = val;
+ * function TreeNode(val) {
+ * Definition for a binary tree node.
+/**
+[
