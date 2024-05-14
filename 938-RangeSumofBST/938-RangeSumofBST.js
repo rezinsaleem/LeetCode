bae8183af4f7326a22ae163b824d1400d737e4ee
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-14 18:25:33 +0530

    Create file

---
+++ 938-RangeSumofBST/938-RangeSumofBST.js
@@ -0,0 +1,15 @@
+            if (root.val >= low && root.val <= high) {
+                console.log(root.val)
+                sum += root.val
+            }
+            inOrder(root.right)
+        }
+        return
+    }
+            inOrder(root.left)
+    function inOrder(root) {
+        if (root) {
+    let sum = 0
+var rangeSumBST = function (root, low, high) {
+ */
+[
