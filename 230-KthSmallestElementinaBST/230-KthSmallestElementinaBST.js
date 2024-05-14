5e3c2d9f3df5bad15bb6deeae88a14350e99a8a7
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-14 10:01:11 +0530

    Create file

---
+++ 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.js
@@ -0,0 +1,13 @@
+    function inOrder(root) {
+        if (root) {
+            inOrder(root.left)
+        }
+            if (n++ < k) res = root.val
+            inOrder(root.right)
+    }
+    let res;
+    let n = 0
+var kthSmallest = function (root, k) {
+ */
+    inOrder(root)
+[
