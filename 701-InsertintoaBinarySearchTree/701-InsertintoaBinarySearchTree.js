c8d8e971e7d470096ba64b07adf8b0ccb2dd46c2
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-15 12:41:10 +0530

    Create file

---
+++ 701-InsertintoaBinarySearchTree/701-InsertintoaBinarySearchTree.js
@@ -0,0 +1,10 @@
+        if (!root.right) {
+    } else {
+            root.right = node
+        } else {
+        }
+            insertNode(root.left, node)
+        } else {
+            root.left = node
+        if (!root.left) {
+[
