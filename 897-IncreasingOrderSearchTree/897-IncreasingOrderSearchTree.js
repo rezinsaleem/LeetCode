50d3418e6d435cea8e28f19ecf4eb7954f3fde97
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-15 19:47:21 +0530

    Create file

---
+++ 897-IncreasingOrderSearchTree/897-IncreasingOrderSearchTree.js
@@ -0,0 +1,12 @@
+        }
+            inOrder(root.right)
+            arr.push(root.val)
+            inOrder(root.left)
+        if (root) {
+    const inOrder = ((root) => {
+    const arr = []
+var increasingBST = function (root) {
+ */
+ * @return {TreeNode}
+ * @param {TreeNode} root
+[
