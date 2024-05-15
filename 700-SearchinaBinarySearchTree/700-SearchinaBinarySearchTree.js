7361b5cee4235e3ade9e77ae486ad6690beae8ff
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-15 12:23:31 +0530

    Create file

---
+++ 700-SearchinaBinarySearchTree/700-SearchinaBinarySearchTree.js
@@ -0,0 +1,11 @@
+        }else{
+        if(root.val == val){
+            return root
+        }else if(root.val>val){
+            root=root.left
+    while(root){
+var searchBST = function(root, val) {
+ */
+ * @return {TreeNode}
+            root = root.right
+[
