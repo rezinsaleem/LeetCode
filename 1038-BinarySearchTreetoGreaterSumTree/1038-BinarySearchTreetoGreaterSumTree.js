769ff25ea0145516b7a6f021a67ec6ce5bb89389
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-15 13:26:47 +0530

    Create file

---
+++ 1038-BinarySearchTreetoGreaterSumTree/1038-BinarySearchTreetoGreaterSumTree.js
@@ -0,0 +1,11 @@
+            inOrder(root.right)
+            sum += root.val
+            inOrder(root.left)
+        }
+            root.val = sum
+        if (root) {
+    let sum = 0
+    const inOrder = ((root) => {
+var bstToGst = function (root) {
+ */
+[
