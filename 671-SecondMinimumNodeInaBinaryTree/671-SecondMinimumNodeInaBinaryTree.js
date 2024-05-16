c4700c09d6ee489ecfc416779ffcae1d0d6dbb82
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-17 00:34:29 +0530

    Create file

---
+++ 671-SecondMinimumNodeInaBinaryTree/671-SecondMinimumNodeInaBinaryTree.js
@@ -0,0 +1,11 @@
+    if (arr[0] == arr[arr.length - 1]) {
+    arr = [...new Set(arr)]
+    inOrder(root)
+    })
+        }
+        return -1
+    }
+    return arr[1]
+    arr.sort((a, b) => a - b)
+};
+[
