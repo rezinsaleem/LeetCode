0ee47bb4903de72eee50a60e5b220ddcfcec6a40
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-18 18:54:00 +0530

    Create file

---
+++ 2399-CheckDistancesBetweenSameLetters/2399-CheckDistancesBetweenSameLetters.js
@@ -0,0 +1,14 @@
+        let pos = s.charCodeAt(i) - 97
+        console.log(count)
+        }
+            j++;
+            }
+                count = j - i
+            if (s[i] == s[j]) {
+        if (count != 0 && count - 1 !== distance[pos]) {
+            res = false;
+        }
+    }
+    return res
+};
+"
