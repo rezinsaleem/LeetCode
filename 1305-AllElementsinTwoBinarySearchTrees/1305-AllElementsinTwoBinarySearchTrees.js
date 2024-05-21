a961404d7ac8c12e2954267c8897794516566058
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-21 13:03:28 +0530

    Create file

---
+++ 1305-AllElementsinTwoBinarySearchTrees/1305-AllElementsinTwoBinarySearchTrees.js
@@ -0,0 +1,15 @@
+        }
+    })
+    inOrder(root1)
+    inOrder(root2)
+    return arr.sort((a, b) => a - b)
+            inOrder(root.left)
+            arr.push(root.val)
+            inOrder(root.right)
+};
+        if (root) {
+    const inOrder = (root => {
+    const arr = []
+var getAllElements = function (root1, root2) {
+ */
+[
