39dc61951c123b569308f57b28fedca6d772d5d1
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-24 11:07:53 +0530

    Update file 5/24/2024, 11:07:54 AM

--- 100-SameTree/100-SameTree.js
+++ 100-SameTree/100-SameTree.js
@@ -1,15 +1,15 @@
-            return [null];
-        if (!root) {
-        }
-        return [root.val, ...preOrder(root.left), ...preOrder(root.right)];
-    }
-
-    let arr1 = preOrder(p);
     let arr2 = preOrder(q);
-  
     if (arr1.length !== arr2.length) {
         return false;
-   const preOrder = (root) => {
-var isSameTree = function (p, q) {
- */
+
+    }
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+};
 [
