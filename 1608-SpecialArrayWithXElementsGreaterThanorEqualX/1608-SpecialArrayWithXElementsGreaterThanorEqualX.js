d6357a7ce8b02d69f9c6aa54b601ea373405faa0
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-27 17:31:52 +0530

    Create file

---
+++ 1608-SpecialArrayWithXElementsGreaterThanorEqualX/1608-SpecialArrayWithXElementsGreaterThanorEqualX.js
@@ -0,0 +1,13 @@
+    for (let x = 0; x <= nums.length; x++) {
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] >= x) res += 1;
+        }
+    }
+        if (res == x) return res
+        let res = 0;
+var specialArray = function (nums) {
+ */
+ * @return {number}
+    return -1;
+};
+[
