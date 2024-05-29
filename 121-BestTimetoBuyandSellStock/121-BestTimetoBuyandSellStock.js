0d4f3888584e0bb00b79cc0c9d3972ef4d72a567
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-29 10:56:50 +0530

    Create file

---
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.js
@@ -0,0 +1,15 @@
+    let maxProfit = 0;         
+    for (let i = 1; i < prices.length; i++) {
+        minPrice = Math.min(minPrice, prices[i]);
+
+     let minPrice = prices[0];
+    let res = [0]
+        maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+    }
+
+    return maxProfit;
+};
+var maxProfit = function (prices) {
+ */
+ * @return {number}
+[
