3c5023788d0dfbae25dfa7ce895ae4406cd1b08c
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-30 10:05:53 +0530

    Create file

---
+++ 66-PlusOne/66-PlusOne.js
@@ -0,0 +1,11 @@
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let digit  = BigInt(digits.join(''))
+/**
+    digit++;
+    let res = digit.toString().split('')
+    return res;
+};
+[
