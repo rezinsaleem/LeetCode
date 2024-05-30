e281f3d32d91ffd992878766773b2f4bc8f6a943
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-05-30 09:34:33 +0530

    Create file

---
+++ 342-PowerofFour/342-PowerofFour.js
@@ -0,0 +1,15 @@
+ * @return {boolean}
+ */
+var isPowerOfFour = function (n) {
+    if (n < 1) {
+        return false;
+    }
+    while (n > 1) {
+        if (n % 4 !== 0) {
+            return false;
+        }
+    }
+        n = n / 4
+    return true;
+};
+1
