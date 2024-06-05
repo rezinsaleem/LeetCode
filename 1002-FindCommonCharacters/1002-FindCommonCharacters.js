4d65693eed55813cbb9cdfd9ab9bf8fae8ede957
rezinsaleem <152444541+rezinsaleem@users.noreply.github.com>
2024-06-06 00:26:36 +0530

    Create file

---
+++ 1002-FindCommonCharacters/1002-FindCommonCharacters.js
@@ -0,0 +1,15 @@
+            words = words.map(word => word.replace(letter, ''))
+        if(words.every(word => word.includes(letter))) {
+            result.push(letter)
+    
+    for(const letter of firstWordArr) {
+    const result = []
+    const firstWordArr = [...words[0]]
+var commonChars = function(words) {
+        }
+ */
+ * @return {string[]}
+    }    
+    
+    return result
+[
