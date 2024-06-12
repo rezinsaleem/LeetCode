735d90856d3307edbbbf265f1e1bdd529ac29c0b
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-06-12 10:32:58 +0530

    Create file

---
+++ 392-IsSubsequence/392-IsSubsequence.js
@@ -0,0 +1,10 @@
+        let idx = t.indexOf(s[i], c + 1);
+        if (idx === -1) {
+            return false;
+        }
+        c = idx;
+    }
+    return true;
+};
+    for (let i = 0; i < s.length; i++) {
+"
