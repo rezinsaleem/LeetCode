e981fca604a9f3d53d1eaf13c15404745f14c7de
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-06-12 09:48:00 +0530

    Update file 6/12/2024, 9:48:01 AM

--- 75-SortColors/75-SortColors.js
+++ 75-SortColors/75-SortColors.js
@@ -1,17 +1,14 @@
-  do {
-      }
-    }    
-    swapped=false;
-    for(i=0;i<arr.length-1;i++){
-      if(arr[i]>arr[i+1]){
-        swapped=true;
-        temp = arr[i];
-        arr[i+1]=temp;
-        arr[i]=arr[i+1];
-  } while (swapped);
-  return arr;
-    let swapped;
-var sortColors = function(arr) {
- */
+        swapped = false;
+        for (i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+/**
+ * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
-[
+ */
+var sortColors = function (arr) {
+    let swapped;
+    do {
+[2,0,2,1,1,0]
