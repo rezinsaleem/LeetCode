c144b78ac5ed404e0d872b25811d4a951acd766c
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-06-22 15:23:41 +0530

    Create file

---
+++ 2665-CounterII/2665-CounterII.js
@@ -0,0 +1,14 @@
+    return {
+        increment: ()=>{
+          return newInit;
+        },
+        decrement: ()=>{
+           newInit = newInit+1 
+            newInit = newInit -1
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let newInit = init 
+5
