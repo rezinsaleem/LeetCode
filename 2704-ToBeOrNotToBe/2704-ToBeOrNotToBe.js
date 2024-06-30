18851d36569a4bf006298376cce56492b5c07195
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-07-01 02:15:46 +0530

    Create file

---
+++ 2704-ToBeOrNotToBe/2704-ToBeOrNotToBe.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function (val) {
+    return {
+};
+            if (val === val1) return true
+        },
+            else throw error = 'Not Equal'
+        notToBe: (val1) => {
+            if (val !== val1) return true
+            else throw error = 'Equal'
+        }
+    }
+        toBe: (val1) => {
+(
