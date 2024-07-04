93f17f488c041cf098e818035520ee4efe0655ed
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-07-04 16:10:24 +0530

    Create file

---
+++ 2666-AllowOneFunctionCall/2666-AllowOneFunctionCall.js
@@ -0,0 +1,10 @@
+var once = function(fn) {
+    let calls = false
+    return function(...args){
+        calls = true;
+    }
+        return fn(...args)
+    return undefined;
+        if(!calls){
+        }
+(
