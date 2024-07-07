5cbc571be397b19f07d09e6845000d2da3dc83c2
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-07-07 23:51:52 +0530

    Create file

---
+++ 1518-WaterBottles/1518-WaterBottles.js
@@ -0,0 +1,9 @@
+    return numBottles + Math.floor((numBottles - 1) / (numExchange - 1));
+var numWaterBottles = function (numBottles, numExchange) {
+ */
+ * @return {number}
+ * @param {number} numExchange
+ * @param {number} numBottles
+/**
+};
+9
