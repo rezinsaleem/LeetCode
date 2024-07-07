e3c925d897598b855a002fe4661e0237da57673e
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-07-08 00:07:50 +0530

    Create file

---
+++ 283-MoveZeroes/283-MoveZeroes.js
@@ -0,0 +1,10 @@
+    }
+        }
+            nums.splice(i, 1)
+        if (nums[i] == 0) {
+    for (let i = 0; i < nums.length; i++) {
+            arr.push(0)
+    console.log(nums, arr)
+    for (i = 0; i < arr.length; i++) {
+            i -= 1
+[
