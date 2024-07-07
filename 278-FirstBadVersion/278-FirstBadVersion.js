c32bbe9854f535a20c7307755670b216c9e6b442
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-07-08 00:21:13 +0530

    Create file

---
+++ 278-FirstBadVersion/278-FirstBadVersion.js
@@ -0,0 +1,10 @@
+            } else {
+                start = mid + 1
+            if (isBadVersion(mid)) {
+                end = mid - 1
+            let mid = Math.floor((start + end) / 2)
+        while (start <= end) {
+        let end = n
+        let start = 1;
+    return function (n) {
+5
