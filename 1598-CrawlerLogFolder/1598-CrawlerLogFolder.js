e314d15b9cea7233747b397d0aaef4224c3f29c7
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-07-10 16:55:52 +0530

    Create file

---
+++ 1598-CrawlerLogFolder/1598-CrawlerLogFolder.js
@@ -0,0 +1,10 @@
+            move += 1;
+        } else if (logs[i] == '../') {
+            if (move > 0) move -= 1
+        }
+    }
+        if (logs[i] !== './' && logs[i] !== '../') {
+    for (let i = 0; i < logs.length; i++) {
+    let move = 0;
+var minOperations = function (logs) {
+[
