a35da9f177f187b545c6b26545af1ab0bb8587cc
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-07-13 11:36:15 +0530

    Create file

---
+++ 3146-PermutationDifferencebetweenTwoStrings/3146-PermutationDifferencebetweenTwoStrings.js
@@ -0,0 +1,9 @@
+        let j = t.indexOf(s[i]);
+        res += Math.abs(i - j)
+    }
+    for (let i = 0; i < s.length; i++) {
+    let res = 0;
+var findPermutationDifference = function (s, t) {
+    return res;
+};
+"
