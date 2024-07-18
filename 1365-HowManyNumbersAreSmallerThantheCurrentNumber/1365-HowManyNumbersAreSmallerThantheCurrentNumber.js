160f327bafc60f9bb997f35331c87fd9db63b37b
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-07-18 20:18:11 +0530

    Create file

---
+++ 1365-HowManyNumbersAreSmallerThantheCurrentNumber/1365-HowManyNumbersAreSmallerThantheCurrentNumber.js
@@ -0,0 +1,15 @@
+ */
+var smallerNumbersThanCurrent = function (nums) {
+        for (let j = 0; j < nums.length; j++) {
+            if (i != j && nums[j] < nums[i]) {
+        }
+    let res = [];
+                count++;
+            }
+    }
+    for (let i = 0; i < nums.length; i++) {
+        res.push(count);
+        let count = 0;
+    return res;
+};
+[
