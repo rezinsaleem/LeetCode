5a30ae6e56c316009b919ae1fbb9a9ba8cff8578
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-07-18 20:02:42 +0530

    Create file

---
+++ 1603-DesignParkingSystem/1603-DesignParkingSystem.js
@@ -0,0 +1,16 @@
+    if (carType === 1) {
+ParkingSystem.prototype.addCar = function (carType) {
+ */
+var ParkingSystem = function (big, medium, small) {
+    this.big = big;
+    this.medium = medium;
+    this.small = small;
+};
+
+/** 
+ * @param {number} carType
+ * @return {boolean}
+ */
+ * @param {number} small
+ * @param {number} medium
+[
