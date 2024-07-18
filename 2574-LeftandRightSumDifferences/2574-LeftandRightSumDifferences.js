7fa74cac5fc65193a834556e187ce9a134e25c57
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-07-18 20:49:56 +0530

    Create file

---
+++ 2574-LeftandRightSumDifferences/2574-LeftandRightSumDifferences.js
@@ -0,0 +1,15 @@
+        for (let j = i - 1; j >= 0; j--) {
+            leftsum += nums[j]
+        }
+        let leftsum = 0;
+        let rightsum = 0;
+        for (let k = i + 1; k < nums.length; k++) {
+            rightsum += nums[k]
+    for (let i = 0; i < nums.length; i++) {
+        }
+        output.push(Math.abs(leftsum - rightsum))
+    }
+    return output;
+    let output = []
+var leftRightDifference = function (nums) {
+[
