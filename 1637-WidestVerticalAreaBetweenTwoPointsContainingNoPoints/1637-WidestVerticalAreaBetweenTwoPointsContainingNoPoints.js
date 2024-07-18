8edd2046f2efc4b1740619f1d8ffdc160e7228a8
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-07-18 20:12:48 +0530

    Create file

---
+++ 1637-WidestVerticalAreaBetweenTwoPointsContainingNoPoints/1637-WidestVerticalAreaBetweenTwoPointsContainingNoPoints.js
@@ -0,0 +1,15 @@
+ * @param {number[][]} points
+ * @return {number}
+ */
+var maxWidthOfVerticalArea = function (points) {
+    points.sort((a, b) => a[0] - b[0]);
+    for (let i = 0; i < points.length - 1; i++) {
+        if (diff > res) {
+    let res = 0;
+            res = diff;
+        }
+    }
+        let diff = points[i + 1][0] - points[i][0];
+    return res
+};
+[
