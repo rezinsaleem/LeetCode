14045073c85e7510d9811a22b3179086704e8e0b
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-07-23 11:24:11 +0530

    Create file

---
+++ 1636-SortArraybyIncreasingFrequency/1636-SortArraybyIncreasingFrequency.js
@@ -0,0 +1,10 @@
+        if (obj[a] === obj[b]) {
+    return nums.sort((a, b) => {
+    nums.forEach(countFunc)
+    }
+        obj[keys] = ++obj[keys] || 1;
+    let countFunc = keys => {
+            return b - a;
+        }
+        return obj[a] - obj[b];
+[
