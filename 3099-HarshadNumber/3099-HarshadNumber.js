992dc82cde39dfe0b2c9d4290508e9dce8585048
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-07-26 00:12:49 +0530

    Create file

---
+++ 3099-HarshadNumber/3099-HarshadNumber.js
@@ -0,0 +1,19 @@
+    let sum = Number(newX[0]) + (newX[1]?Number(newX[1]):0)
+    let newX = x.toString().split('')
+var sumOfTheDigitsOfHarshadNumber = function (x) {
+    if ( x % sum == 0) {
+        return sum
+    }
+    return -1;
+ */
+ * @return {number}
+1
+18
+23
+1
+9
+-1
+1
+9
+-1
+1
