7d98a8ea8e01caf405fe770f0970a848f792dfa3
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-07-27 21:06:21 +0530

    Create file

---
+++ 2520-CounttheDigitsThatDivideaNumber/2520-CounttheDigitsThatDivideaNumber.js
@@ -0,0 +1,18 @@
+    for (let i = 0; i < nums.length; i++) {
+        if (num % Number(nums[i]) === 0) {
+    let res = 0;
+            res++
+        }
+    }
+    return res;
+};
+7
+7
+121
+1248
+1
+2
+4
+1
+2
+4
