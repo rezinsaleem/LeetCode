663baca79ef5a2577300a7c6389a51dba3af6362
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-07-31 11:18:22 +0530

    Create file

---
+++ 3232-FindifDigitGameCanBeWon/3232-FindifDigitGameCanBeWon.js
@@ -0,0 +1,19 @@
+            double += nums[i]
+        } else {
+            single += nums[i]
+        }
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i].toString().split('').length > 1) {
+    let double = 0
+    let single = 0
+var canAliceWin = function (nums) {
+[
+[1,2,3,4,10]
+[1,2,3,4,5,14]
+[5,5,5,25]
+false
+true
+true
+false
+true
+true
