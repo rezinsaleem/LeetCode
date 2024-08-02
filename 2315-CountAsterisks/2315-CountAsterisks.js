b1bf04aed6622aa7a462e254d19b2b10f34af974
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-08-02 11:44:43 +0530

    Create file

---
+++ 2315-CountAsterisks/2315-CountAsterisks.js
@@ -0,0 +1,19 @@
+    let green=true, count=0;
+    for(let i=0; i<s.length; i++){
+        if(green && s[i]=="*"){count++};
+        if(s[i]=="|"){green=!green};
+    }
+    return count;
+var countAsterisks = function(s) {
+ */
+};
+"
+"l|*e*et|c**o|*de|"
+"iamprogrammer"
+"yo|uar|e**|b|e***au|tifu|l"
+2
+0
+5
+2
+0
+5
