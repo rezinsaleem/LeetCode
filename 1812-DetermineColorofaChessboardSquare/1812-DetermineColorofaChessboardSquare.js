3bac28d299484456fc4fd1d452f845e4e812435c
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-08-06 19:53:53 +0530

    Create file

---
+++ 1812-DetermineColorofaChessboardSquare/1812-DetermineColorofaChessboardSquare.js
@@ -0,0 +1,16 @@
+    }
+    sum = Number(obj[coordinates[0]]) + Number(coordinates[1])
+        h: 8
+    if (sum % 2 == 0) return false;
+    else return true;
+};
+"
+"a1"
+"h3"
+"c7"
+false
+true
+false
+false
+true
+false
