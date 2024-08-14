057ecf7191761f141c8d1166466035681752dc6e
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-08-14 15:59:42 +0530

    Create file

---
+++ 709-ToLowerCase/709-ToLowerCase.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function (s) {
+    return s.toLowerCase()
+};
+"
+"Hello"
+"here"
+"LOVELY"
+"hello"
+"here"
+"lovely"
+"hello"
+"here"
+"lovely"
