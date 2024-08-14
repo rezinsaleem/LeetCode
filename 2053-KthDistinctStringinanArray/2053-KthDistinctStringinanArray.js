cc8d9517129c29eca6183f2b1b707a2d783a83f9
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-08-14 16:17:06 +0530

    Create file

---
+++ 2053-KthDistinctStringinanArray/2053-KthDistinctStringinanArray.js
@@ -0,0 +1,18 @@
+        }
+        if (count == 1) res.push(arr[i])
+    }
+    return res[k - 1] ?? '';
+};
+[
+["d","b","c","b","c","a"]
+2
+["aaa","aa","a"]
+1
+["a","b","a"]
+3
+"a"
+"aaa"
+""
+"a"
+"aaa"
+""
