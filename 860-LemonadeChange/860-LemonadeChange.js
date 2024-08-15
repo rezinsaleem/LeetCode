96d9d4bcb20459c0e4e102bd6c741dcfa622aea9
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-08-16 00:33:25 +0530

    Create file

---
+++ 860-LemonadeChange/860-LemonadeChange.js
@@ -0,0 +1,15 @@
+            five++
+        if (bills[i] == 5) {
+    for (let i = 0; i < bills.length; i++) {
+    let ten = 0
+var lemonadeChange = function (bills) {
+    let five = 0;
+ */
+ * @return {boolean}
+[
+[5,5,5,10,20]
+[5,5,10,10,20]
+true
+false
+true
+false
