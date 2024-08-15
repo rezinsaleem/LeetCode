dddf13fe2920198b927d3707c8ea2cbfbc746ffe
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-08-15 12:25:48 +0530

    Create file

---
+++ 1684-CounttheNumberofConsistentStrings/1684-CounttheNumberofConsistentStrings.js
@@ -0,0 +1,22 @@
+            if (allowed.includes(w[j])) {
+        for (let j = 0; j < w.length; j++) {
+        w = words[i].split('')
+        let flag;
+    let res = 0;
+var countConsistentStrings = function (allowed, words) {
+ */
+    for (let i = 0; i < words.length; i++) {
+                flag = true;
+"
+"ab"
+["ad","bd","aaab","baa","badab"]
+"abc"
+["a","b","c","ab","ac","bc","abc"]
+"cad"
+["cc","acd","b","ba","bac","bad","ac","d"]
+2
+7
+4
+2
+7
+4
