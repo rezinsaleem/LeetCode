667bed7e7d7af5eb363ea49b3cc2d4433c461499
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-08-19 20:09:27 +0530

    Create file

---
+++ 2788-SplitStringsbySeparator/2788-SplitStringsbySeparator.js
@@ -0,0 +1,19 @@
+        if (words[j] !== '') {
+            res.push(words[j])
+        }
+    }
+    return res;
+};
+[
+["one.two.three","four.five","six"]
+"."
+["$easy$","$problem$"]
+"$"
+["|||"]
+"|"
+["one","two","three","four","five","six"]
+["easy","problem"]
+[]
+["one","two","three","four","five","six"]
+["easy","problem"]
+[]
