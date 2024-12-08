321778eac9fe0f0310eff320590ad3d6cb6f1c61
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-12-08 15:42:19 +0530

    Create file

---
+++ 1232-CheckIfItIsaStraightLine/1232-CheckIfItIsaStraightLine.js
@@ -0,0 +1,10 @@
+var checkStraightLine = function(coordinates) {
+    let a = -(coordinates[1][1] - coordinates[0][1]);
+    let b = coordinates[1][0] - coordinates[0][0];
+    return coordinates.every(
+        item =>
+            a * (item[0] - coordinates[0][0]) +
+                b * (item[1] - coordinates[0][1]) ===
+            0
+    );
+};
