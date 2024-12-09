6ba7e82eb46b494421ef85d93160c0db3f9477be
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-12-09 20:54:13 +0530

    Create file

---
+++ 500-KeyboardRow/500-KeyboardRow.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findWords = function(words) {
+    const rows = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm']
+    
+    return words.filter(word => rows.some(row => [...word.toLowerCase()].every(char => row.includes(char))))
+};
