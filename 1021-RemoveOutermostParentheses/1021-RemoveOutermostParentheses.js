f3d3014cf8be7888d8bcc6cbdce85129f03fe953
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2024-12-23 15:08:22 +0530

    Create file

---
+++ 1021-RemoveOutermostParentheses/1021-RemoveOutermostParentheses.js
@@ -0,0 +1,20 @@
+var removeOuterParentheses = function(S) {
+    let parenthesCount = 0;
+    let result = "";
+    
+    for (const letter of S) {
+        if (letter === "(") {
+            if (parenthesCount) {
+                result += letter;
+            }
+            parenthesCount++;
+        } else {
+            parenthesCount--;
+            if (parenthesCount) {
+                result += letter;
+            }
+        }
+    }
+    
+    return result;
+};
