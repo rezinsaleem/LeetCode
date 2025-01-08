f87c00cfd1c81ce9f827d3ab788abbc053d04dab
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2025-01-08 12:14:05 +0530

    Create file

---
+++ 3042-CountPrefixandSuffixPairsI/3042-CountPrefixandSuffixPairsI.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var countPrefixSuffixPairs = function (words) {
+    let count = 0;
+    for (let i = 0; i < words.length-1; i++) {
+        for (let j = i + 1; j < words.length; j++) {
+            if (words[j].startsWith(words[i]) && words[j].endsWith(words[i])) {
+                count++;
+            }
+        }
+    }
+    return count;
+};
