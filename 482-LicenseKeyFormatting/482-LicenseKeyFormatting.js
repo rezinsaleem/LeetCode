71fd25b77d30657e38fdbdf548313689bbc1ef31
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2025-01-21 19:41:15 +0530

    Create file

---
+++ 482-LicenseKeyFormatting/482-LicenseKeyFormatting.js
@@ -0,0 +1,9 @@
+var licenseKeyFormatting = function(s, k) {
+    s = s.replaceAll('-', '').toUpperCase().split('');
+    
+    for(let i = s.length - k; i > 0; i -= k) {
+        s[i] = `-${s[i]}`
+    }
+    
+    return s.join('');
+};
