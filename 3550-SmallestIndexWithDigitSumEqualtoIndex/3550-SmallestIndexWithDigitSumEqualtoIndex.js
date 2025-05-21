8ff71135ab30dec5cc36cb60161c032c11ba9371
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2025-05-21 16:06:40 +0530

    File updated at 5/21/2025, 4:06:40 PM

--- 3550-SmallestIndexWithDigitSumEqualtoIndex/3550-SmallestIndexWithDigitSumEqualtoIndex.js
+++ 3550-SmallestIndexWithDigitSumEqualtoIndex/3550-SmallestIndexWithDigitSumEqualtoIndex.js
@@ -1,16 +1,13 @@
-// Last updated: 5/21/2025, 4:01:28 PM
+// Last updated: 5/21/2025, 4:06:40 PM
 /**
  * @param {number[]} nums
  * @return {number}
  */
-var smallestIndex = function(nums) {
-    for(let i=0;i<nums.length;i++){
+var smallestIndex = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
         let sumOfDigits = nums[i].toString().split('')
-        if(sumOfDigits.length>1){
-            let sum = sumOfDigits.reduce((acc,num)=>acc+Number(num),0)
-            if(i==sum) return i
-        }
-        if(i==sumOfDigits) return i
+        let sum = sumOfDigits.reduce((acc, num) => acc + Number(num), 0)
+        if (i == sum) return i
     }
     return -1;
 };
