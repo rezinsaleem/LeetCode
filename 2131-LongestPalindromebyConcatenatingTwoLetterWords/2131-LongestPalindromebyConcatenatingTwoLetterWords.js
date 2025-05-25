f209d19b52aaa79b51aff390237f3a68073c43cf
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2025-05-25 17:45:55 +0530

    Create 2131-LongestPalindromebyConcatenatingTwoLetterWords

---
+++ 2131-LongestPalindromebyConcatenatingTwoLetterWords/2131-LongestPalindromebyConcatenatingTwoLetterWords.js
@@ -0,0 +1,39 @@
+// Last updated: 5/25/2025, 5:45:55 PM
+var longestPalindrome = function (words) {
+    let res = 0;
+    let diff = new Map();
+    let same = new Map();
+    let usedMiddle = false;
+
+    // Count frequencies
+    for (let w of words) {
+        if (w[0] === w[1]) {
+            same.set(w, (same.get(w) || 0) + 1);
+        } else {
+            diff.set(w, (diff.get(w) || 0) + 1);
+        }
+    }
+
+    // Handle diff-letter reversible pairs like "ab" + "ba"
+    for (let [w, freq] of diff) {
+        let rev = w[1] + w[0];
+
+        if (diff.has(rev)) {
+            let pairCount = Math.min(freq, diff.get(rev));
+            res += pairCount * 4;
+            diff.set(w, 0);      // mark used
+            diff.set(rev, 0);    // mark used
+        }
+    }
+
+    // Handle same-letter words like "ff", "oo"
+    for (let [w, freq] of same) {
+        res += Math.floor(freq / 2) * 4;
+        if (freq % 2 === 1 && !usedMiddle) {
+            res += 2;
+            usedMiddle = true;
+        }
+    }
+
+    return res;
+};
