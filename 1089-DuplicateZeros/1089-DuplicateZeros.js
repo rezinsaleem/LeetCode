4403173a0f810bbdde4d29359fc406a911da49af
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2025-06-01 15:36:45 +0530

    File updated at 6/1/2025, 3:36:45 PM

--- 1089-DuplicateZeros/1089-DuplicateZeros.js
+++ 1089-DuplicateZeros/1089-DuplicateZeros.js
@@ -1,16 +1,15 @@
-// Last updated: 6/1/2025, 3:32:47 PM
+// Last updated: 6/1/2025, 3:36:45 PM
 /**
  * @param {number[]} arr
  * @return {void} Do not return anything, modify arr in-place instead.
  */
-var duplicateZeros = function(arr) {
-    let arr1 = []
+var duplicateZeros = function (arr) {
     let n = arr.length
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]===0){
-            arr.splice(i+1,0,0)
-            i++
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            arr.splice(i + 1, 0, 0)
+            i++;
+            arr.pop()
         }
     }
-    arr.splice(n)
 };
