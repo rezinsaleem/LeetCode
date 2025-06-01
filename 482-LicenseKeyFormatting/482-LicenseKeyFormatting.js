cac3f022760c2f13d5e36c19edf2e7623c511675
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2025-06-01 16:10:18 +0530

    File updated at 6/1/2025, 4:10:18 PM

--- 482-LicenseKeyFormatting/482-LicenseKeyFormatting.js
+++ 482-LicenseKeyFormatting/482-LicenseKeyFormatting.js
@@ -1,5 +1,12 @@
+// Last updated: 6/1/2025, 4:10:18 PM
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
 var licenseKeyFormatting = function(s, k) {
     s = s.replaceAll('-', '').toUpperCase().split('');
+    console.log(s)
     
     for(let i = s.length - k; i > 0; i -= k) {
         s[i] = `-${s[i]}`
