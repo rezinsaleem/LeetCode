f7ed4a7f844d1c3c6f79c291b19eda74ad26b7b6
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2025-06-16 15:02:27 +0530

    Create 2016-MaximumDifferenceBetweenIncreasingElements

---
+++ 2016-MaximumDifferenceBetweenIncreasingElements/2016-MaximumDifferenceBetweenIncreasingElements.js
@@ -0,0 +1,17 @@
+// Last updated: 6/16/2025, 3:02:26 PM
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumDifference = function (nums) {
+    let max = -1
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] - nums[i] > max) {
+                max = nums[j] - nums[i]
+            }
+        }
+    }
+    if (max == 0) return -1;
+    else return max;
+};
