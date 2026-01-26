203bb32d136384bde3ea63dd8803c3809b2ab1d4
Rezin Saleem <152444541+rezinsaleem@users.noreply.github.com>
2026-01-27 00:55:59 +0530

    File updated at 1/27/2026, 12:56:01 AM

--- 1200-MinimumAbsoluteDifference/1200-MinimumAbsoluteDifference.js
+++ 1200-MinimumAbsoluteDifference/1200-MinimumAbsoluteDifference.js
@@ -1,14 +1,22 @@
-    const result = []
-        if (arr[i + 1] - arr[i] < mindiff) {
-            mindiff = arr[i + 1] - arr[i]
-        }
-    }
-    for (let i = 1; i < arr.length - 1; i++) {
-    arr = arr.sort((a, b) => a - b);
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i + 1] - arr[i] == mindiff) {
-            result.push([arr[i], arr[i + 1]])
-    let mindiff = arr[1] - arr[0]
-var minimumAbsDifference = function (arr) {
- */
-[
+// Last updated: 1/27/2026, 12:56:01 AM
+1/**
+2 * @param {number[]} arr
+3 * @return {number[][]}
+4 */
+5
+6var minimumAbsDifference = function (arr) {
+7    arr = arr.sort((a, b) => a - b);
+8    let min = arr[1] - arr[0]
+9    for (let i = 1; i < arr.length - 1; i++) {
+10        if (arr[i + 1] - arr[i] < min) {
+11            min = arr[i + 1] - arr[i]
+12        }
+13    }
+14    const result = []
+15    for (let i = 0; i < arr.length; i++) {
+16        if (arr[i + 1] - arr[i] == min) {
+17            result.push([arr[i], arr[i + 1]])
+18        }
+19    }
+20    return result
+21};
